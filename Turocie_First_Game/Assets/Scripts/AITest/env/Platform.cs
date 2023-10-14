e4845330ff6fc3fc002ffd87fdb01d3823d8fbd9
EgeDogantemur <egedogantemur26@gmail.com>
2023-10-14 23:47:33 +0300

    Merge branch 'Master' into Design-Art
    asdasd

---
+++ Turocie_First_Game/Assets/Scripts/AITest/env/Platform.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+
+    [SerializeField]
+    public Transform posA, posB;
+
+    [SerializeField]
+    public float Speed;
+
+    [Header("DetectionBox Settings")]
+    [SerializeField] GameObject _detectionBox;
+    [SerializeField] Vector3 _detectionBoxSize;
+    public bool isInteractionAvalible = false;
+
+
+    Vector3 targetPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPos = posB.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Vector2.Distance(transform.position, posA.position) < 0.1f) targetPos = posB.position;
+        if (Vector2.Distance(transform.position, posB.position) < 0.1f) targetPos = posA.position;
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPos , Speed ); 
+
+    }
+
+    private void FixedUpdate()
+    {
+        Collider2D[] colls = Physics2D.OverlapBoxAll(
+            _detectionBox.transform.position, 
+            _detectionBoxSize , 
+            _detectionBox.transform.localEulerAngles.z
+        );
+
+        isInteractionAvalible = false;
+        foreach(Collider2D coll in colls)
+        {
+            if(coll != null)
+            {
+                isInteractionAvalible = true;
+                Rigidbody2D rb;
+                if(coll.TryGetComponent(out rb))
+                {
+                    var direction = Vector3.zero;
+                    if (Vector3.Distance(transform.position, targetPos) > 1f)
+                    {
+                        direction = targetPos - transform.position;
+                        coll.transform.parent.transform.parent = this.gameObject.transform;
+                    }
+
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+
+        if(_detectionBox != null && _detectionBoxSize != null)
+        {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireCube(_detectionBox.transform.position, _detectionBoxSize);
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
